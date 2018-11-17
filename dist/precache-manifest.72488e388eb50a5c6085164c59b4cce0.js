eb6b8df33841bd734deef21bec556059c5944b22
praveen-me <praveenkrsaini0@gmail.com>
2018-11-17 18:18:09 +0530

    Update README.md

---
+++ dist/precache-manifest.72488e388eb50a5c6085164c59b4cce0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4dc6f7dc610c61bcfc8c",
+    "url": "/static/js/main.4dc6f7dc.chunk.js"
+  },
+  {
+    "revision": "35dc91afd4ba2dc89438",
+    "url": "/static/js/1.35dc91af.chunk.js"
+  },
+  {
+    "revision": "4dc6f7dc610c61bcfc8c",
+    "url": "/static/css/main.b702884e.chunk.css"
+  },
+  {
+    "revision": "c1ea70befc77d50b9906952965c22663",
+    "url": "/index.html"
+  }
+];
